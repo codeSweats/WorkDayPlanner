ca086744a139dc3edcde64628b10b3a5b546552d
G Code <seanlfrancis@gmail.com>
2021-03-26 01:34:27 -0500

    forgotten updates to javascript and css

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
 // Current time on page load
-var today = moment();
-$('#currentDay').text(today.format("MMM Do, YYYY"));
+var calendar = null
+
+
+
+
+
+
+
+
+
+
+
+
+function setUp() {
+    calendar = localStorage.getItem("calendar");
+    if (calendar) {
+        calendar = JSON.parse(calendar);
+        displayCalendar();
+    }
+    else {
+        calendar = ["", "", "", "", "", "", "", ""];
+        saveCalendar();
+    }
+    $("div[type=button]").on("click", handleBtn);
+}
+
+function saveCalendar() {
+    localStorage.setItem("calendar", JSON.stringify(calendar));
+
+}
+
+function displayCalendar() {
+    var nowHr = moment().hour();
+    $("input[type=text]").each(function (index) {
+        $(this).val(calendar[index]);
+        var tod = +$(this).attr("tod");
+        if(tod < nowHr) {
+            $(this).css("background-color", "gray");
+        } else if(tod > nowHr) {
+            $(this).css("background-color", "green");
+        } else {
+            $(this).css("background-color", "red");
+        }
+    });
+}
+
+function handleBtn() {
+    $("input[type=text]").each(function (index) {
+        calendar[index] = $(this).val()
+    });
+    saveCalendar();
+
+}
+
+
+
+
+
+function setCurrentDate() {
+    var today = moment();
+    $('#currentDay').text(today.format("MMM Do, YYYY"));
+}
+
+setCurrentDate();
+setUp();
